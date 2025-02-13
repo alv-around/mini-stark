90b3854481de801955759e6204c5a8c5a1fbefe4
alv-around <alfmail@pm.me>
2025-02-13 11:54:24 +0100

    calculate logarithm of 2^k

--- src/util.rs
+++ src/util.rs
@@ -1,16 +1,23 @@
-use ark_ff::FftField;
 use core::ops::{BitAnd, Sub};
 use num_traits::{One, Zero};
-use std::cmp::PartialEq;
 
 pub fn is_power_of_two<T>(number: T) -> bool
 where
-    T: BitAnd<Output = T> + Sub<Output = T> + PartialEq + Copy + One + Zero,
+    T: BitAnd<Output = T> + Sub<Output = T> + PartialOrd + Copy + One + Zero,
 {
-    number & (number - T::one()) == T::zero()
+    if number.gt(&T::one()) {
+        return number & (number - T::one()) == T::zero();
+    }
+    false
 }
 
-pub fn get_nth_root<F: FftField>(nth_root: u64) -> F {
-    assert!(is_power_of_two(nth_root));
-    FftField::get_root_of_unity(nth_root).unwrap()
+pub fn logarithm_of_two_k<'a, const N: usize>(number: usize) -> Result<usize, &'a str> {
+    if !is_power_of_two(number) {
+        return Err("number if not a power of 2");
+    }
+    let power_of_two = number.trailing_zeros() as usize;
+    if power_of_two % N != 0 {
+        return Err("number if not a power of base");
+    }
+    Ok(power_of_two / N)
 }
