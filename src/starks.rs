23c76c7ddd52f6c2baf1e6fd1e8698fda500173b
alv-around <alfmail@pm.me>
2025-04-17 00:09:42 +0200

    STARKs: add logging

--- src/starks.rs
+++ src/starks.rs
@@ -43,22 +43,44 @@ where
     D: Digest + FixedOutputReset + BlockSizeUser + Clone,
 {
     pub fn new(config: StarkConfig<D, F>) -> Self {
+        let StarkConfig {
+            steps,
+            security_bits,
+            blowup_factor,
+            rounds,
+            ..
+        } = config;
+        info!(
+            "\n
+            --------- \n 
+            New STARK with following configuration:\n
+            trace length: {} | security bits: {} | blowup factor: {} | rounds: {}\n
+            --------- \n",
+            steps, security_bits, blowup_factor, rounds
+        );
         Self(config)
     }
+
     pub fn prove<T, AIR: Provable<T, F>>(
         &self,
         air: AIR,
         witness: T,
     ) -> Result<StarkProof<D, F>, Box<dyn Error>> {
         let mut merlin = self.0.io.to_merlin();
-        // 1. compute trace polys and commit to them
+        info!("Proving: start proving...");
+        // 1. compute trace and commit to trace
         let trace = air.trace(&witness);
         let trace_domain = trace.get_domain();
         let trace_codeword =
             MerkleTree::<D, F>::new(trace.trace.get_data(), self.0.merkle_config.clone());
         let trace_commit = trace_codeword.root();
         merlin.add_bytes(&trace_commit).unwrap();
+        debug!(
+            "Proving: 1. original trace ({:?}) committed",
+            trace_commit.to_ascii_lowercase()
+        );
 
+        // 2. calculate low degree extension of the original trace
         let lde_domain_size = self.0.blowup_factor * trace_domain.size();
         let [random_shift]: [F; 1] = merlin.challenge_scalars().unwrap();
         let lde_domain = Radix2EvaluationDomain::new(lde_domain_size)
@@ -75,10 +97,22 @@ where
             MerkleTree::<D, F>::new(constrain_trace.get_data(), self.0.merkle_config.clone());
         let constrain_trace_commit = constrain_trace_codeword.root();
         merlin.add_bytes(&constrain_trace_commit).unwrap();
+        debug!(
+            "domain size: {} | lde domain size: {} | blowup factor: {}",
+            trace_domain.size(),
+            lde_domain_size,
+            self.0.blowup_factor
+        );
+        debug!(
+            "Proving: 2. constrain trace ({:?}) committed",
+            constrain_trace_commit.to_ascii_lowercase()
+        );
 
-        // parametric batching g = f_0 + r f_1 + r^2 f_2 + .. + r^(n-1) f_{n-1}
+        // 3. mix constrains polynomial into the validity polynomial
         let r: [F; 1] = merlin.challenge_scalars().unwrap();
+        debug!("random variable for mixing r: {:?}", r);
         let mut mixed_constrain_poly = DensePolynomial::<_>::from_coefficients_vec(vec![F::ZERO]);
+        // parametric batching g = f_0 + r f_1 + r^2 f_2 + .. + r^(n-1) f_{n-1}
         for (i, constrain_poly) in constrains.get_polynomials().iter().enumerate() {
             mixed_constrain_poly = mixed_constrain_poly
                 + DensePolynomial::<_>::from_coefficients_vec(vec![r[0].pow([i as u64])])
@@ -87,7 +121,9 @@ where
         let (rest, validity_poly) = mixed_constrain_poly.divide_by_vanishing_poly(trace_domain);
         assert_eq!(rest, DensePolynomial::zero());
         let validity_lde = validity_poly.clone().evaluate_over_domain(lde_domain);
+        debug!("Proving: 3. validity poly from mixing constrains",);
 
+        // 4. commit to the validity polynomial
         let mut validity_trace = Matrix::<F>::new(lde_domain_size, 1, None);
         validity_trace.add_col(0, validity_lde.evals);
         let validity_codeword = MerkleTree::<D, F>::new(
@@ -95,34 +131,51 @@ where
             self.0.fri_config.merkle_config.clone(),
         );
         let validity_commit = validity_codeword.root();
+        debug!(
+            "Proving: 4. validity trace ({:?}) committed",
+            constrain_trace_commit.to_ascii_lowercase()
+        );
 
-        // 2. Queries
+        // 5. Proving: receive random queries from verifier and prove the queries correspond to the
+        //    previous work. This include to main steps:
         let mut query_bytes = vec![0u8; 8 * self.0.constrain_queries];
         merlin.fill_challenge_bytes(&mut query_bytes).unwrap();
         let queries = query_bytes
             .chunks_exact_mut(8)
             .map(|bytes| usize::from_le_bytes(bytes.try_into().unwrap()) % lde_domain_size)
             .collect::<Vec<_>>();
+        debug!(
+            "Proving: 5. draw {} trace queries",
+            self.0.constrain_queries
+        );
 
+        // 5.1 Link the committed constrain trace to the validity trace
         let mut constrain_queries = Vec::new();
         let mut validity_queries = Vec::new();
         for query in queries.into_iter() {
             // constrain queries
             let leaf = constrain_trace.get_value(query, 0);
-            let path = constrain_trace_codeword.generate_proof(leaf).unwrap();
-            constrain_queries.push(path);
+            match constrain_trace_codeword.generate_proof(leaf) {
+                Ok(path) => constrain_queries.push(path),
+                Err(err) => error!("{}", err),
+            }
 
             // validity query
             let leaf = validity_trace.get_value(query, 0);
-            let path = validity_codeword.generate_proof(leaf).unwrap();
-            validity_queries.push(path);
+            match validity_codeword.generate_proof(leaf) {
+                Ok(path) => validity_queries.push(path),
+                Err(err) => error!("{}", err),
+            }
         }
+        debug!("Proving: 5.1 queries to committed traces provided");
 
-        // 3. Make the low degree test FRI
+        // 5.2 Make the low degree test FRI on the validity polynomial
         let prover = FriProver::<D, _>::new(&mut merlin, validity_poly, self.0.fri_config.clone());
         let fri_commit = prover.get_initial_commit();
         let (fri_proof, _) = prover.prove();
+        debug!("Proving: 5.2 FRI test proved");
 
+        info!("Proving: Finished successfully!");
         let arthur = merlin.transcript().to_vec();
         Ok(StarkProof {
             arthur,
@@ -137,6 +190,7 @@ where
     }
 
     pub fn verify(&self, constrains: Constrains<F>, proof: StarkProof<D, F>) -> bool {
+        info!("Verification: start verification...");
         let StarkProof {
             arthur,
             trace_commit,
@@ -147,6 +201,7 @@ where
             fri_commit,
             fri_proof,
         } = proof;
+        // 1. assert proof commits match transcript and calculate coset
         let mut arthur: Arthur<'_, DigestBridge<D>, u8> = self.0.io.to_arthur(&arthur);
         assert_eq!(arthur.next_digest().unwrap(), trace_commit);
 
@@ -155,30 +210,30 @@ where
         assert_eq!(arthur.next_digest().unwrap(), constrain_trace_commit);
 
         let [r]: [F; 1] = arthur.challenge_scalars().unwrap();
+        debug!("Verification: 1. proof commits match transcript");
 
-        // 2. run queries
+        // 2. build validity polynomial assert it matches the the query values provided in
+        //    the proof
         let lde_domain = Radix2EvaluationDomain::<F>::new(domain.size() * self.0.blowup_factor)
             .unwrap()
             .get_coset(shift)
             .unwrap();
-        let zerofier = domain
-            .vanishing_polynomial()
-            .evaluate_over_domain(lde_domain);
-        println!("Zerofier evals: {:?}", zerofier.evals);
+
         let mut query_bytes = vec![0u8; 8 * self.0.constrain_queries];
         arthur.fill_challenge_bytes(&mut query_bytes).unwrap();
         let queries = query_bytes
             .chunks_exact_mut(8)
             .map(|bytes| usize::from_le_bytes(bytes.try_into().unwrap()) % lde_domain.size())
             .collect::<Vec<_>>();
+        debug!("Verification: 2.1 queries from transcript retrieved");
 
-        let validity_root = MerkleRoot::<D>(validity_commit.clone());
+        // 2.2 assert that the queries provided from constrain trace match commit
+        // build with this queries the validity polynomial
+        let validity_root = MerkleRoot::<D>(validity_commit);
         let quotient_root = MerkleRoot::<D>(constrain_trace_commit);
-        let query_num = 0;
         for (query, (constrain_query, validity_query)) in
             zip(queries, zip(constrain_queries, validity_queries))
         {
-            println!("query num: {}", query_num);
             let mut c_x = DensePolynomial::zero();
             let mut leafs = Vec::new();
             let w_i = lde_domain.element(query);
@@ -199,13 +254,16 @@ where
             assert!(validity_query.leaf_neighbours.contains(&evaluation));
             assert!(validity_root.check_proof(validity_query));
         }
+        debug!("Verification: 2.2 linking between validity and constrain polynomials successfull");
 
         // 3. run fri
         let fri_root = MerkleRoot::<D>(fri_commit);
         let fri_verifier =
             FriVerifier::<D, F>::new(fri_root, self.0.degree, self.0.fri_config.clone());
         assert!(fri_verifier.verify(fri_proof, &mut arthur));
+        debug!("Verification: 3. FRI verification passed");
 
+        info!("Verification: proof verification completed");
         true
     }
 }
@@ -219,7 +277,9 @@ where
 {
     #[allow(dead_code)]
     security_bits: usize,
+    steps: usize,
     blowup_factor: usize,
+    rounds: usize,
     constrain_queries: usize,
     degree: usize,
     fri_config: FriConfig<D, F>,
@@ -245,14 +305,11 @@ where
         let degree = steps - 1;
         let rounds = ceil_log2_k((steps * blowup_factor) + 1, 2);
 
-        info!(
-            "--------- \n 
-            New STARK prover with following configuration:\n
-            "
-        );
         Self {
             security_bits,
+            steps,
             blowup_factor,
+            rounds,
             constrain_queries,
             degree,
             fri_config: FriConfig {
